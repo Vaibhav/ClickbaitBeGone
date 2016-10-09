bdfaaf04d746c22c1c1a60f15501fb5a652e1db3
Vaibhav <ishuvaibhav@gmail.com>
2016-10-09 00:07:52 -0400

    I broke it somehow.

--- code/js/content.js
+++ code/js/content.js
@@ -19,16 +19,57 @@
 
 const listOfRegex = [
 	/you won['’]?t believe/i,
-	/what happens next/i, 
+	/what happens next/i,
+	/A Woman Had A/i,
+	/^\d+ (\w+ )?(animals|pictures|lessons|movies|secrets|shows|stories|things|times|trailers|tumblr|tweets)/i,
+	/^\d+ of the \w+est/i,
+	/^can you/i,
+	/^here['’]s (how|what)/i,
+	/^this could/i,
+	/all (he|she|they) did was/i,
+	/all the best/i,
+	/best comeback/i,
+	/can['’]t handle/i,
+	/can teach us about/i,
+	/didn['’]t know what/i,
+	/get rid of/i,
+	/how one (man|woman)/i,
+	/how \w+ are you/i,
+	/may affect/i,
+	/never realized/i,
+	/(pictures|photos) of/i,
+	/secret of/i,
+	/signs you['’]?re/i,
+	/somebody needs to/i,
+	/things that will/i,
+	/trump/i,
+	/until you see/i,
+	/you (need to|should) (know|watch)/i,
+	/we bet you can/i,
+	/we can (tell|guess) (what )?your/i,
+	/we need to talk about/i,
+	/what could possibly/i,
+	/what happens/i,
+	/what (he|she|they) found/i,
+	/what I learned about/i,
+	/what this/i,
+	/what to expect/i,
+	/when (he|she|they)/i,
+	/when this (man|woman|baby|child|puppy|dog|kitten)/i,
+	/when you read these/i,
+	/who['’]d thougt/i,
+	/why we really shouldn['’]?t/i,
+	/with this one/i,
+	/will never tell/i,
+	/won['’]?t believe/i,
+	/\s(celebrit|epic|fantastic|genius|heartbreaking|incredibl|powerful|shocking|teen|terribl|unusual|weirdly)/i 
 ];
 
 function initObserver() {
 	const observer = new MutationObserver( function(mutations) {
 
 		mutations.forEach( function (mut) {
-
 			mut.addedNodes.forEach( function (node) {
-
 				if (node.nodeType === Node.ELEMENT_NODE){
 					recurseClickbaitLinks(node);
 				}
@@ -71,10 +112,10 @@ function changeClickbait(item)
 	}
 }
 
-function recurseClickbaitLinks(item) {
+function recurseClickbaitLinks(element) {
 
 	// Check all links for Clickbait titles
-	items = item.getElementsByTagName('a');
+	items = element.getElementsByTagName('a');
 	//use spread operator and pass each of the items through clickbait checker
 	[...items].forEach(changeClickbait);
 }
